6cfc07f096fb2a385d26f7ec1bc91f2ae86e66b2
mamen <mamen@users.noreply.github.com>
2019-03-26 20:12:03 +0100

    update

--- simpleOS/main.c
+++ simpleOS/main.c
@@ -1,51 +1,30 @@
 #include <msp430.h>
 #include "os.h"
+#include "scheduler.h"
 
-
-uint8_t getThreadID() {
-    int i;
-    for(i = 0; i < MAX_THREADS; i++) {
-        if(threadList[i] == NULL) {
-            threadList[i] = &t;
-            return i;
-        }
-    }
-
-    return THREAD_ID_INVALID;
+void ATOMIC_START(void) {
+    // Disable Interrupts
+    _disable_interrupts();
 }
 
-void scheduler_killThread() {
-
+void ATOMIC_END(void) {
+    // Enable Interrupts
+    _enable_interrupts();
 }
 
-uint8_t scheduler_startThread(void* (*func)()) {
-
-    Thread *t = malloc(sizeof(Thread));
-
-    t->threadID = getThreadID();
-
-    if(t->threadID == THREAD_ID_INVALID) {
-        return -1;
-    }
-
-    t->state = THREAD_READY;
-    t->threadFunc = func;
-
-    if(setjmp(threadList[t->threadID].context) == 0) {
-        return t->threadID;
-    } else {
-        threadList[currentRunningThread]->threadFunc(currentRunningThread);
-        scheduler_killThread();
-        return THREAD_ID_INVALID;
+void blink_green() {
+    while(1) {
+        P1OUT |= 1 << 0;
+        P1OUT &= ~1 << 0;
     }
 }
 
-
-void test(uint8_t tID) {
-    int a = tID;
-    int b = a * 4;
+void blink_red() {
+    while(1) {
+        P1OUT |= 1 << 6;
+        P1OUT &= ~1 << 6;
+    }
 }
-
 
 
 /**
@@ -53,10 +32,15 @@ void test(uint8_t tID) {
  */
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	WDTCTL = WDTPW | WDTHOLD;
 	
+	_enable_interrupts();
 
-	scheduler_startThread(&test);
+
+	os_createThread(blink_green);
+	os_createThread(blink_red);
+
+    os_start();
 
 	return 0;
 }
--- simpleOS/os.h
+++ simpleOS/os.h
@@ -1,31 +1,34 @@
 #ifndef OS_H_
 #define OS_H_
 
-#include <setjmp.h>
 #include <inttypes.h>
+#include <setjmp.h>
 
-#define THREAD_ID_INVALID -1
 #define MAX_THREADS 10
 
-typedef enum {
+typedef enum State {
     THREAD_READY,
     THREAD_RUNNING
-} ThreadState;
+} State_t;
 
 typedef struct {
-    uint8_t threadID;
+    uint16_t stack;
     jmp_buf context;
-    void* (*threadFunc)();
-    ThreadState state;
-} Thread;
+    State_t state;
 
+    void(*func)();
+} Thread_t;
 
-Thread* threadList[MAX_THREADS];
-uint8_t currentRunningThread;
+Thread_t threads[MAX_THREADS];
+int thread_counter = 0;
+Thread_t* running = 0;
 
+void ATOMIC_START(void);
+void ATOMIC_END(void);
 
-uint8_t scheduler_startThread(void* (*threadFunc)());
-
+void os_createThread(void(*action)(Thread_t*));
+Thread_t* os_round_robin(void);
+void os_change_thread(Thread_t* thread);
+void os_start();
 
 #endif
-
